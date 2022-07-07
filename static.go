2fac215893b5c845f271b518cade94b21aa6067a
arpachuilo <andrew.pachuilo@gmail.com>
2022-07-07 19:01:51 -0400

    using etags on static assets, for now . . .

--- static.go
+++ static.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"embed"
+	"fmt"
 	"io/fs"
 	"net/http"
+	"runtime/debug"
 
 	"github.com/arpachuilo/go-registerable"
 )
@@ -12,17 +14,42 @@ import (
 var staticFS embed.FS
 
 type CachedFileServer struct {
-	fs http.Handler
+	fs   http.Handler
+	etag *string
 }
 
 func (self CachedFileServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Cache-Control", "public, max-age=7776000")
+	// w.Header().Set("Cache-Control", "public, max-age=7776000")
+	// check etag
+	etag := r.Header.Get("If-None-Match")
+	if etag == *self.etag {
+		w.WriteHeader(http.StatusNotModified)
+		return
+	}
+
+	// set etag
+	if self.etag != nil {
+		w.Header().Set("ETag", *self.etag)
+	}
+
 	self.fs.ServeHTTP(w, r)
 }
 
 func NewCachedFileServer(fsys fs.FS) CachedFileServer {
 	httpFS := http.FileServer(http.FS(fsys))
-	return CachedFileServer{httpFS}
+	var etag *string
+
+	info, ok := debug.ReadBuildInfo()
+	if ok {
+		for _, s := range info.Settings {
+			if s.Key == "vcs.revision" {
+				v := fmt.Sprintf("1%v", s.Value)
+				etag = &v
+			}
+		}
+	}
+
+	return CachedFileServer{httpFS, etag}
 }
 
 func (self Router) ServeStatic() registerable.Registration {
